1e159daefbeb25fe0607e3acdace98f79b34055b
Maricel Dragan <42100507+MARICEL30@users.noreply.github.com>
2021-12-14 21:33:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+//toggle list, clicking the buttom at the top
+
+let btn = document.getElementById("btn");
+let maintablecontainer = document.getElementById("main-table");
+
+btn.addEventListener("click", function() {
+  console.log(maintablecontainer);
+  maintablecontainer.classList.toggle("open");
+});
+
+
+
+//making the list with google sheets and an api service
+
+const mytable = document.getElementById("myTable");
+// let data = [];
+let url = 'https://api.sheety.co/3d408acfbea2c1fd2fc688d1b38bdd28/plasticFreeme/sheet1';
+
+function getRows() {
+  fetch(url)
+ .then(response => response.json())
+  .then(data => {
+     //console.log(data);
+    data["sheet1"].forEach((row) => {
+      const sheetRows = document.createElement("tr");
+
+      let myshop = document.createElement("td");
+          myshop.innerHTML = row["shop"];
+
+      let mywebsite = document.createElement("td");
+          mywebsite.innerHTML = row["website"];
+
+
+      let myaddress = document.createElement("td");
+          myaddress.innerHTML = row["address"];
+
+
+      sheetRows.appendChild(myshop);
+      sheetRows.appendChild(mywebsite);
+      sheetRows.appendChild(myaddress);
+      mytable.append(sheetRows);
+    
+    });
+  });
+ 
+}
+
+window.addEventListener("load", getRows);
+
+
+
+//creating the google map and positioning the markers for every shop
+
+function initMap() {
+  const brighton = { lat:50.888212725751195 , lng: -0.20438173195189485 };
+   const map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "576aba331fc3dae5",
+    zoom: 12,
+    center: brighton,
+  });
+
+   new google.maps.Marker({
+    position: { lat: 50.86721503284581, lng: -0.1237472981288431 },
+    map,
+    title: "Roots and Hoots",
+    icon: {
+      url:"images/shop-svg.svg",
+      scaledSize:  new google.maps.Size(38, 31)
+    }
+  });
+
+  new google.maps.Marker({
+    position: { lat: 50.82512876580611, lng:-0.1509608153426121  },
+    map,
+    title: "The Source Bulk Foods Brighton",
+    icon: {
+      url:"images/shop-svg.svg",
+      scaledSize:  new google.maps.Size(38, 31)
+    }
+  });
+
+  new google.maps.Marker({
+    position: { lat: 50.84290045197619, lng:-0.13464338622990235 },
+    map,
+    title: "Store Brighton",
+    icon: {
+      url:"images/shop-svg.svg",
+      scaledSize:  new google.maps.Size(38, 31)
+    }
+  });
+
+  new google.maps.Marker({
+    position: { lat: 50.82902551424537,  lng:-0.13559555739461235 },
+    map,
+    title: "HISBE Food",
+    icon: {
+      url:"images/shop-svg.svg",
+      scaledSize:  new google.maps.Size(38, 31)
+    }
+  });
+
+  new google.maps.Marker({
+    position: { lat: 50.8316584829648,  lng:-0.13596955924413806 },
+    map,
+    title: "Wastenot",
+    icon: {
+      url:"images/shop-svg.svg",
+      scaledSize:  new google.maps.Size(38, 31)
+    }
+  });
+
+
+}
+
+
+
+
+
+
